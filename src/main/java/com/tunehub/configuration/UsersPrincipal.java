a07788edc386e1e12582607f538c3ad7d802e4e5
pranay <npranay9899@gmail.com>
2025-02-16 22:25:16 +0530

    implemented Role Based Authentication

--- src/main/java/com/tunehub/configuration/UsersPrincipal.java
+++ src/main/java/com/tunehub/configuration/UsersPrincipal.java
@@ -9,13 +9,16 @@ import java.util.Collections;
 
 public class UsersPrincipal implements UserDetails {
     Users user;
-    public UsersPrincipal(Users user) {
+    Collection<? extends GrantedAuthority> authorities;
+
+    public UsersPrincipal(Users user, Collection<? extends GrantedAuthority> authorities) {
         this.user = user;
+        this.authorities = authorities;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(user.getRole());
+        return this.authorities;
     }
 
     @Override
--- src/main/java/com/tunehub/service/securityservice/MyUserDetailService.java
+++ src/main/java/com/tunehub/service/securityservice/MyUserDetailService.java
@@ -1,12 +1,18 @@
 package com.tunehub.service.securityservice;
 
 import com.tunehub.configuration.UsersPrincipal;
+import com.tunehub.entity.Role;
 import com.tunehub.entity.Users;
 import com.tunehub.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MyUserDetailService implements UserDetailsService {
@@ -19,11 +25,19 @@ public class MyUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Users user = userRepository.findByEmail(username);
-        if(user == null){
-            System.out.println("Username not found");
-            throw new UsernameNotFoundException(username);
+        Users user = userRepository.findByEmail(username); // Will throw UsernameNotFoundException if not found
+
+        Role role = user.getRole();
+        List<GrantedAuthority>  grantedAuthorities = new ArrayList<>();
+
+        if(role != null){
+            String roleType = role.name();
+            grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_"+roleType));
+        } else {
+            System.out.println("WARNING: User '" + username + "' has NO ROLE assigned in database.");
         }
-        return new UsersPrincipal(user);
+
+        System.out.println("Loaded user: "+username+" "+grantedAuthorities);
+        return new UsersPrincipal(user, grantedAuthorities);
     }
 }
